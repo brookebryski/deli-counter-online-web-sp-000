53234e82a7fd5e51bbe92b0386fedaa3162fac8d
chic-state-0895 <brookeabryski@gmail.com>
2020-09-01 21:27:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+  array.each do |index|
+    puts "The line is currently #{index + 1}"
+end
+
+def take_a_number(katz_deli,name)
+end
